48b8a71e58d55ba902e134d9f09cd6a22d7357ff
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2023-05-21 19:23:33 +0530

    Code added

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,34 @@
-import React from 'react'
-import '../styles/App.css';
-import like from '../like.svg';
+import React from "react";
+import "../styles/App.css";
+import like from "../like.svg";
 
 const App = () => {
-  const [count,setCount] = React.useState();
-  
-  return (
-    <div id="main">
-      <img id="like-btn-img" style={{fill:'white',width:'100px',backgroundColor:'rgba(255,0,0,0)'}} />
-      <h3>Likes: <span id="like-counter">0</span></h3>
-    </div>
-  )
-}
+    const [count, setCount] = React.useState(0);
+    const [bgCount, setBgCount] = React.useState(0);
+    const clickHandler = () => {
+        setCount(count + 1);
+        setBgCount(bgCount + 0.1);
+    };
 
+    return (
+        <div id="main">
+            <img
+                id="like-btn-img"
+                style={{
+                    fill: "white",
+                    width: "100px",
+                    backgroundColor: `rgba(255,0,0,${bgCount})`,
+                }}
+                src={like}
+                onClick={() => {
+                    clickHandler();
+                }}
+            />
+            <h3>
+                Likes: <span id="like-counter">{count}</span>
+            </h3>
+        </div>
+    );
+};
 
 export default App;
